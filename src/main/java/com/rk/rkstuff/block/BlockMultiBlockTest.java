1172566253a71f36f5ffcdae4ad7b76d0a91ed98
Rictas <b.giehle@gmail.com>
2015-05-28 19:25:45 +0200

    more work

--- src/main/java/com/rk/rkstuff/block/BlockMultiBlockTest.java
+++ src/main/java/com/rk/rkstuff/block/BlockMultiBlockTest.java
@@ -3,11 +3,20 @@ package com.rk.rkstuff.block;
 import com.rk.rkstuff.tile.TileMultiBlock;
 import com.rk.rkstuff.tile.TileMultiBlockTest;
 import com.rk.rkstuff.util.Reference;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.IIcon;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
-public class BlockMultiBlockTest extends BlockMultiBlock {
+public class BlockMultiBlockTest extends BlockRK implements ITileEntityProvider {
+
+    private IIcon[] icons = new IIcon[2];
 
     public BlockMultiBlockTest() {
         super(Material.iron);
@@ -17,6 +26,26 @@ public class BlockMultiBlockTest extends BlockMultiBlock {
     @Override
     public TileEntity createNewTileEntity(World world, int meta) {
         return new TileMultiBlockTest();
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister) {
+        icons[0] = iconRegister.registerIcon(getUnwrappedUnlocalizedName(this.getUnlocalizedName()));
+        icons[1] = iconRegister.registerIcon(getUnwrappedUnlocalizedName(this.getUnlocalizedName()) + "2");
+        blockIcon = icons[0];
+    }
+
+    @Override
+    public IIcon getIcon(IBlockAccess world, int x, int y, int z, int side) {
+        super.getIcon(world, x, y, z, side);
+        TileEntity tile = world.getTileEntity(x, y, z);
+        if(tile != null && tile instanceof TileMultiBlock) {
+            if(((TileMultiBlock) tile).hasMaster()) {
+                return icons[1];
+            }
+        }
+        return icons[0];
     }
 
 }

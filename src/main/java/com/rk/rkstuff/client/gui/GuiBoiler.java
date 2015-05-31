ece967cfeeaa19a468e5f8a9c08db0dfb4baace0
kieba <kieba4@gmail.com>
2015-05-31 19:50:55 +0200

    logic

--- src/main/java/com/rk/rkstuff/client/gui/GuiBoiler.java
+++ src/main/java/com/rk/rkstuff/client/gui/GuiBoiler.java
@@ -1,9 +1,7 @@
 package com.rk.rkstuff.client.gui;
 
 import com.rk.rkstuff.container.ContainerBoiler;
-import com.rk.rkstuff.container.ContainerSolar;
 import com.rk.rkstuff.tile.TileBoilerBaseMaster;
-import com.rk.rkstuff.tile.TileSolarMaster;
 import com.rk.rkstuff.util.Textures;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
@@ -30,15 +28,15 @@ public class GuiBoiler extends GuiContainer {
         int y = (this.height - SIZE_Y) / 2;
         drawTexturedModalRect(x, y, 0, 0, SIZE_X,  SIZE_Y);
 
-        int coolCoolantPx = Math.round(MAX * 0.5f);
-        int hotCoolantPx = Math.round(MAX * 0.5f);
-        int waterPx = Math.round(MAX * 0.5f);
-        int steamPx = Math.round(MAX * 0.5f);
-        int heatPx = Math.round(MAX * 0.5f);
+        int coolCoolantPx = Math.round(MAX * (float) tile.getCoolCoolantStorage() / (float) tile.getMaxCoolantStorage());
+        int hotCoolantPx = Math.round(MAX * (float) tile.getHotCoolantStorage() / (float) tile.getMaxCoolantStorage());
+        int waterPx = Math.round(MAX * (float) tile.getWaterStorage() / (float) tile.getMaxWaterStorage());
+        int steamPx = Math.round(MAX * (float) tile.getSteamStorage() / (float) tile.getMaxSteamStorage());
+        int heatPx = Math.round(MAX * (float) tile.getTemperature() / (float) tile.getMaxTemperature());
 
         drawTexturedModalRect(x + 21, y + 71 - hotCoolantPx, 192, MAX - hotCoolantPx, 16, hotCoolantPx);
         drawTexturedModalRect(x + 44, y + 71 - coolCoolantPx, 224, MAX - coolCoolantPx, 16, coolCoolantPx);
-        drawTexturedModalRect(x + 82, y + 71 - heatPx, 240, MAX - heatPx, 12, waterPx);
+        drawTexturedModalRect(x + 82, y + 71 - heatPx, 240, MAX - heatPx, 12, heatPx);
         drawTexturedModalRect(x + 116, y + 71 - waterPx, 176, MAX - waterPx, 16, waterPx);
         drawTexturedModalRect(x + 139, y + 71 - steamPx, 208, MAX - steamPx, 16, steamPx);
 
@@ -48,31 +46,31 @@ public class GuiBoiler extends GuiContainer {
             //draw tooltip cool coolant
             List<String> list = new ArrayList<>(2);
             list.add("Hot Coolant:");
-            list.add(String.format("%d/%d mB", 1, 1000));
+            list.add(String.format("%d/%d mB", tile.getHotCoolantStorage(), tile.getMaxCoolantStorage()));
             this.func_146283_a(list, mouseX, mouseY);
         } else if(isInArea(mouseX, mouseY, x + 44, yMin, x + 60, yMax)) {
             //draw tooltip hot coolant
             List<String> list = new ArrayList<>(2);
             list.add("Cool Coolant:");
-            list.add(String.format("%d/%d mB", 1, 1000));
+            list.add(String.format("%d/%d mB", tile.getCoolCoolantStorage(), tile.getMaxCoolantStorage()));
             this.func_146283_a(list, mouseX, mouseY);
         } else if(isInArea(mouseX, mouseY, x + 82, yMin, x + 94, yMax)) {
             //draw tooltip prod
             List<String> list = new ArrayList<>(2);
             list.add("Heat:");
-            list.add(String.format("%d °C", 1, 1000));
+            list.add(String.format("%d °C", tile.getTemperature()));
             this.func_146283_a(list, mouseX, mouseY);
         } else if(isInArea(mouseX, mouseY, x + 116, yMin, x + 132, yMax)) {
             //draw tooltip prod
             List<String> list = new ArrayList<>(2);
             list.add("Water:");
-            list.add(String.format("%d/%d mB", 1, 1000));
+            list.add(String.format("%d/%d mB", tile.getWaterStorage(), tile.getMaxWaterStorage()));
             this.func_146283_a(list, mouseX, mouseY);
         } else if(isInArea(mouseX, mouseY, x + 139, yMin, x + 155, yMax)) {
             //draw tooltip prod
             List<String> list = new ArrayList<>(2);
             list.add("Steam:");
-            list.add(String.format("%d/%d mB", 1, 1000));
+            list.add(String.format("%d/%d mB", tile.getSteamStorage(), tile.getMaxSteamStorage()));
             this.func_146283_a(list, mouseX, mouseY);
         }
     }

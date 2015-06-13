861c7abc22f03119dcae4a2b39536e54d37959a2
kieba <kieba4@gmail.com>
2015-06-13 13:51:31 +0200

    added tileTanks

--- src/main/java/com/rk/rkstuff/block/BlockTankValve.java
+++ src/main/java/com/rk/rkstuff/block/BlockTankValve.java
@@ -4,7 +4,7 @@ import com.rk.rkstuff.util.Reference;
 import net.minecraft.block.material.Material;
 
 public class BlockTankValve extends BlockRK {
-    public BlockTankValve(Material material, String blockName) {
+    public BlockTankValve() {
         super(Material.iron, Reference.BLOCK_TANK_VALVE);
     }
 }

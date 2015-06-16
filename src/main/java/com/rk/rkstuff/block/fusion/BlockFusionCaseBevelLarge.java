bb68956e6caeb377df65ded76017401b419654ee
Rictas <b.giehle@gmx.de>
2015-06-16 21:17:34 +0200

    work work work

--- src/main/java/com/rk/rkstuff/block/fusion/BlockFusionCaseBevelLarge.java
+++ src/main/java/com/rk/rkstuff/block/fusion/BlockFusionCaseBevelLarge.java
@@ -6,7 +6,9 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 public class BlockFusionCaseBevelLarge extends BlockFusionCase {
 
@@ -43,6 +45,16 @@ public class BlockFusionCaseBevelLarge extends BlockFusionCase {
     @Override
     public IIcon getIcon(int side, int meta) {
         return blockIcon;
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+        if (!world.isRemote) {
+            int meta = world.getBlockMetadata(x, y, z);
+            meta = (meta + 1) % 12;
+            world.setBlockMetadataWithNotify(x, y, z, meta, 3);
+        }
+        return true;
     }
 
 }
--- src/main/java/com/rk/rkstuff/block/fusion/BlockFusionCaseBevelSmallInverted.java
+++ src/main/java/com/rk/rkstuff/block/fusion/BlockFusionCaseBevelSmallInverted.java
@@ -6,7 +6,9 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IIcon;
+import net.minecraft.world.World;
 
 public class BlockFusionCaseBevelSmallInverted extends BlockFusionCase {
 
@@ -45,4 +47,13 @@ public class BlockFusionCaseBevelSmallInverted extends BlockFusionCase {
         return blockIcon;
     }
 
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+        if (!world.isRemote) {
+            int meta = world.getBlockMetadata(x, y, z);
+            meta = (meta + 1) % 8;
+            world.setBlockMetadataWithNotify(x, y, z, meta, 3);
+        }
+        return true;
+    }
 }
